563143d244437dc9589b958f5a2bdf2201881a64
ronydiu0 <asadcse100@gmail.com>
2023-09-25 09:59:13 +0600

    update

--- @core/resources/views/components/summernote/js.blade.php
+++ @core/resources/views/components/summernote/js.blade.php
@@ -1,25 +1,46 @@
 <script src="{{asset('assets/backend/js/summernote-bs4.js')}}"></script>
 <script>
     (function($){
-    "use strict";
-    $(document).ready(function () {
-        $('.summernote').summernote({
-            height: 200,   //set editable area's height
-            codemirror: { // codemirror options
-                theme: 'monokai'
-            },
-            callbacks: {
-                onChange: function(contents, $editable) {
-                    $(this).prev('input').val(contents);
+            "use strict";
+            $(document).ready(function () {
+                // Initialize Summernote editor
+                $('.summernote').summernote({
+                    height: 300,   // Set editable area's height
+                    codemirror: { // Codemirror options
+                        theme: 'monokai'
+                    },
+                    callbacks: {
+                        onChange: function(contents, $editable) {
+                            $(this).prev('input').val(contents);
+                        }
+                    },
+                    toolbar: [
+                        ['style', ['style']],
+                        ['font', ['bold', 'italic', 'underline', 'clear']],
+                        ['fontname', ['fontname']],
+                        ['color', ['color']],
+                        ['para', ['ul', 'ol', 'paragraph']],
+                        ['height', ['height']],
+                        ['table', ['table']],
+                        ['insert', ['link', 'picture', 'hr']],
+                        ['view', ['fullscreen', 'codeview']],
+                        ['help', ['help']]
+                    ],
+                    buttons: {
+                        codeBlock: function () {
+                            // Add a code block template when the button is clicked
+                            const codeBlockTemplate = '<pre><code>// Your code goes here</code></pre>';
+                            $('.summernote').summernote('pasteHTML', codeBlockTemplate);
+                        },
+                    },
+                });
+
+                // Load content if multiple editors exist
+                if ($('.summernote').length > 1) {
+                    $('.summernote').each(function (index, value) {
+                        $(this).summernote('code', $(this).data('content'));
+                    });
                 }
-            }
-        });
-        if($('.summernote').length > 1){
-            $('.summernote').each(function(index,value){
-                $(this).summernote('code', $(this).data('content'));
             });
-        }
-    });
-            
-    })(jQuery);        
+        })(jQuery);
 </script>
